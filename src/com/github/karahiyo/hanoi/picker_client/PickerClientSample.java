2958926525c1a37fe8ed2ce1ac5067b7bca311d5
karahiyo <a.ryuklnm@gmail.com>
2013-11-09 11:06:31 +0900

    modify UDP socket program.

--- src/com/github/karahiyo/hanoi/picker_client/PickerClientSample.java
+++ src/com/github/karahiyo/hanoi/picker_client/PickerClientSample.java
@@ -1,14 +1,12 @@
 package com.github.karahiyo.hanoi.picker_client;
 
-
 public class PickerClientSample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		PickerClient client = new PickerClient();
-		client.setHost(args[0]);
-		client.setPORT(9999);
-		for(int i=0; i<1000; i++) {
-			client.send("てすてす" + (int)(i*Math.random()*100%7));
+		for(int i=0; i<100; i++) {
+			client.send("test message" + (int)(i*Math.random()*100%7));
 		}
+		client.socketClose();
 	}
 }
