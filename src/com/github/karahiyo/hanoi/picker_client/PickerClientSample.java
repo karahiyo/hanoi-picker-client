c7782685f47292a78b1df266ed63c640fb0f5fff
karahiyo <a.ryuklnm@gmail.com>
2013-11-11 21:23:42 +0900

    can send message each map/shuffle phase.

--- src/com/github/karahiyo/hanoi/picker_client/PickerClientSample.java
+++ src/com/github/karahiyo/hanoi/picker_client/PickerClientSample.java
@@ -5,7 +5,7 @@ public class PickerClientSample {
 	public static void main(String[] args) throws Exception{
 		PickerClient client = new PickerClient();
 		for(int i=0; i<1; i++) {
-			client.send("test_" + (int)(i*Math.random()*100%7));
+			client.send("MAP" + "," + "test_" + (int)(i*Math.random()*100%7));
 		}
 		client.socketClose();
 	}
